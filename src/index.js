4df067c569773f7c5980bb80db9f1ec6cc4da695
Nah0my <rodorrego09@gmail.com>
2023-11-03 01:45:17 -0300

    Agregando dependencias y cifrado para el token

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import { Client, GatewayIntentBits } from "discord.js";
+// Importa las bibliotecas necesarias
+import { ActivityType, Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
 
+// Lee las variables de entorno desde el archivo .env
+dotenv.config();
+
+// Define las variables que necesitas
+const prefix = "!";
+
+// Permisos que posee el cliente de discord y definiendo al mismo
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-const TOKEN =
-  "MTE2NTEwMzI0MjM4MDg1MzMyMg.GWZnTk.2yHQ6_CmzRddEDI4yCzCvFRs-9flPXXmqsN1ZU";
+// Comprobando que el bot se conecto con exito
+client.once("ready", () => {
+  console.log(`${client.user.username} esta en linea`);
 
-client.login(TOKEN);
+  // Configurando la presencia del bot
+  client.user.setActivity({
+    name: "si es que podemos hablar", // El mensaje que quieres mostrar
+    type: ActivityType.Watching, // Que se encuentra haciendo el bot, si leyendo, jugando, viendo, etc.
+  });
+});
+
+// Inicia sesión en Discord utilizando el token de las variables de entorno
+client.login(process.env.TOKEN);
